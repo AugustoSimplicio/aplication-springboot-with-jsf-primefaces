c29ce33898129742baf9939e47b3aa3209fab8c5
Intera <Intera@100.64.0.202>
2019-10-23 12:59:31 -0300

    Commit projeto nao esta renderisando as paginas.

--- ProjetoEstagio/src/main/java/br/com/augusto/ProjetoEstagioApplication.java
+++ ProjetoEstagio/src/main/java/br/com/augusto/ProjetoEstagioApplication.java
@@ -1,6 +1,8 @@
 package br.com.augusto;
 
 import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,7 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
@@ -29,29 +32,35 @@ public class ProjetoEstagioApplication extends SpringBootServletInitializer impl
 		FacesServlet servlet = new FacesServlet();
 		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.jsf");
 		return servletRegistrationBean;
-			
+
 	}
-	
+
+	@Override
+	public void onStartup(final ServletContext servletContext) throws ServletException {
+	}
+
 	@Bean
-	   public ServletContextInitializer servletContextInitializer() {
-	return sc -> {
-	           sc.setInitParameter("APP_NAME", "ProjetoEstagio");
-//	            sc.setInitParameter("com.sun.faces.expressionFactory", "com.sun.el.ExpressionFactoryImpl");
+	public ServletContextInitializer servletContextInitializer() {
+		return sc -> {
+			sc.setInitParameter("APP_NAME", "ProjetoEstagio");
+			sc.setInitParameter("com.sun.faces.expressionFactory",
+					"org.springframework.web.jsf.el.SpringBeanFacesElResolver");
 //	            sc.setInitParameter("facelets.DEVELOPMENT", Boolean.TRUE.toString());
-	           sc.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
-	           sc.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
-	           sc.setInitParameter("javax.faces.DEFAULT_SUFFIX", ".xhtml");
-	           sc.setInitParameter("javax.faces.FACELETS_REFRESH_PERIOD", "1");
-	           sc.setInitParameter("javax.faces.FACELETS_SKIP_COMMENTS", Boolean.TRUE.toString());
-	           sc.setInitParameter("javax.faces.PARTIAL_STATE_SAVING_METHOD", Boolean.TRUE.toString());
-	           sc.setInitParameter("javax.faces.STATE_SAVING_METHOD", "server");
-	           sc.setInitParameter("primefaces.CLIENT_SIDE_VALIDATION", Boolean.TRUE.toString());
-	           sc.setInitParameter("primefaces.FONT_AWESOME", Boolean.TRUE.toString());
-	           
-//	            sc.addListener(new ContextLoaderListener());
-	           sc.addListener(new RequestContextListener());
-	           
-	           sc.addFilter("openSessionInView",  new OpenEntityManagerInViewFilter()).addMappingForUrlPatterns(null, false, "/*");
-	       };
+			sc.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+			sc.setInitParameter("javax.faces.PROJECT_STAGE", "Development");
+			sc.setInitParameter("javax.faces.DEFAULT_SUFFIX", ".xhtml");
+			sc.setInitParameter("javax.faces.FACELETS_REFRESH_PERIOD", "1");
+			sc.setInitParameter("javax.faces.FACELETS_SKIP_COMMENTS", Boolean.TRUE.toString());
+			sc.setInitParameter("javax.faces.PARTIAL_STATE_SAVING_METHOD", Boolean.TRUE.toString());
+			sc.setInitParameter("javax.faces.STATE_SAVING_METHOD", "server");
+			sc.setInitParameter("primefaces.CLIENT_SIDE_VALIDATION", Boolean.TRUE.toString());
+			sc.setInitParameter("primefaces.FONT_AWESOME", Boolean.TRUE.toString());
+
+			sc.addListener(new ContextLoaderListener());
+			sc.addListener(new RequestContextListener());
+
+			sc.addFilter("openSessionInView", new OpenEntityManagerInViewFilter()).addMappingForUrlPatterns(null, false,
+					"/*");
+		};
 	}
 }
